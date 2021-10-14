e7d75763d7ec96d8f1e2498ef7ec37a58be64331
sriaknth <143srikanthnaiu@gmail.com>
2021-10-14 13:03:22 +0530

    jar file

---
+++ MAVEN/HDFC/fundstransfer/fundstransfer/src/main/java/hdfc/hdfcExtension.java
@@ -0,0 +1,29 @@
+package hdfc;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.RouteDefinition;
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.syndesis.integration.runtime.api.SyndesisExtensionAction;
+
+@Configuration
+public class hdfcExtension {
+    @Bean
+    @SyndesisExtensionAction(
+        id = "my-step",
+        name = "My Step",
+        description = "A simple step",
+        entrypoint = "direct:my-step"
+    )
+    public RouteBuilder myAction() {
+        return new RouteBuilder() {
+            @Override
+            public void configure() {
+                from("direct:my-step")
+                    .log("Body is: ${body}");
+            }
+        };
+    }
+}
